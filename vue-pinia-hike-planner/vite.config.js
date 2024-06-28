21d806364353ee1ac4c12735b51f31d9d42169ec
S-Andor <serban.andor01@gmail.com>
2024-06-28 16:33:57 +0300

    reinit project to use javascript

--- vue-pinia-hike-planner/vite.config.js
+++ vue-pinia-hike-planner/vite.config.js
@@ -2,20 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom'
   }
 })
